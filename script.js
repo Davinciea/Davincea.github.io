a27f107ea096e6ab29c8255d77e507c2ceb1b4fc
Davinciea <mberrehail@mgail.com>
2018-12-16 12:50:56 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+function temps(text) {
+  setTimeout(cache, 10000);
+  }
+
+ 
+function cache() {
+alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+  }
+
+function couleur() {
+	var couleur = document.getElementById("coul");
+	var couleurBody = document.getElementById("container");
+	couleurBody.style.backgroundColor = "magenta";
+
+}
+
+function normal() {
+	var normal = document.getElementById("normal");
+	var normalBody = document.getElementById("container");
+    normalBody.style.backgroundColor = ""
+}
+
+
